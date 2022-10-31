8f71e8448fb79648192f8531d72c44078dc09e4e
Alex N <alexnoriega9@gmail.com>
2022-10-31 15:47:09 -0600

    Changed syntax of app initialization

--- public/app.js
+++ public/app.js
@@ -13,8 +13,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
+
 // Refernece contactInfo collections
 let contactInfo = firebase.database().ref("infos");
 
