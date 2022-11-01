961610d8f2e2b284775cb2d558588dd67284eb40
Alex N <alexnoriega9@gmail.com>
2022-10-31 23:52:16 -0600

    Added Email and retrieve info code

--- public/app.js
+++ public/app.js
@@ -26,11 +26,13 @@ function submitForm(e) {
   let name = document.querySelector(".name").value;
   let email = document.querySelector(".email").value;
   let message = document.querySelector(".message").value;
-  console.log(name, email, message);
+  //console.log(name, email, message);
 
   saveContactInfo(name, email, message);
 
   document.querySelector(".contact-form").reset();
+
+  sendEmail(name, email, message);
 }
 
 // Save infos to Firebase
@@ -42,4 +44,48 @@ function saveContactInfo(name, email, message) {
     email: email,
     message: message,
   });
+  retrieveInfos();
+}
+
+// Retrieve Infos
+function retrieveInfos() {
+  let ref = firebase.database().ref("infos");
+  ref.on("value", gotData);
+}
+
+function gotData(data) {
+  let info = data.val();
+  let keys = Object.keys(info);
+
+  for (let i = 0; i < keys.length; i++) {
+    let infoData = keys[i];
+    let name = info[infoData].name;
+    let email = info[infoData].email;
+    let message = info[infoData].message;
+    //console.log(name, email, message);
+
+    let infosResults = document.querySelector(".infosResults");
+
+    infosResults.innerHTML += `<div>
+    <p><strong>Name: </strong>${name} <br>
+    <a><strong>Email: </strong>${email} <br>
+    <a><strong>Message: </strong>${message} <br>
+    </p>
+    </div>`
+  }
+}
+
+retrieveInfos();
+
+// Send Email Info
+function sendEmail(name, email, message) {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "alexnoriega9@gmail.com",
+    Password: "pktpobyjthvvqsmd",
+    To: "alex_noriega9@hotmail.com",
+    From: "alexnoriega9@gmail.com",
+    Subject: `${name} sent you a message`,
+    Body: `Name: ${name} <br/> Email: ${email} <br/> Message: ${message}`,
+  }).then((message) => alert("Mail sent successfully"))
 }
